afdb214bd97989eaf8006f667a8687a8bb5ecd28
shterneregen <shterneregen@gmail.com>
2020-12-02 23:45:21 +0300

    one more hash example

--- src/main/java/random/hash/HashUtils.java
+++ src/main/java/random/hash/HashUtils.java
@@ -1,40 +1,57 @@
 package random.hash;
 
+import org.apache.commons.codec.binary.Hex;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class HashUtils {
 
-    private static final String SHA_ALGORITHM = "SHA-256";
+	private static final Logger LOG = Logger.getLogger(HashUtils.class.getName());
 
-    private HashUtils() {
-    }
+	private static final String SHA_ALGORITHM = "SHA-256";
 
-    public static byte[] generateRandomSalt() {
-        byte[] salt = new byte[16];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(salt);
-        return salt;
-    }
+	private HashUtils() {
+	}
 
-    public static byte[] createSha2Hash(String input, byte[] salt) throws Exception {
-        ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
-        byteStream.write(salt);
-        byteStream.write(input.getBytes());
-        byte[] valueToHash = byteStream.toByteArray();
-        MessageDigest messageDigest = MessageDigest.getInstance(SHA_ALGORITHM);
-        return messageDigest.digest(valueToHash);
-    }
+	public static byte[] generateRandomSalt() {
+		byte[] salt = new byte[16];
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.nextBytes(salt);
+		return salt;
+	}
 
-    public static String hashPassword(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt());
-    }
+	public static byte[] createSha2Hash(String input, byte[] salt) throws Exception {
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		byteStream.write(salt);
+		byteStream.write(input.getBytes());
+		byte[] valueToHash = byteStream.toByteArray();
+		MessageDigest messageDigest = MessageDigest.getInstance(SHA_ALGORITHM);
+		return messageDigest.digest(valueToHash);
+	}
 
-    public static boolean verifyPassword(String password, String hashedPassword) {
-        return BCrypt.checkpw(password, hashedPassword);
-    }
+	public static String hashPassword(String password) {
+		return BCrypt.hashpw(password, BCrypt.gensalt());
+	}
 
+	public static boolean verifyPassword(String password, String hashedPassword) {
+		return BCrypt.checkpw(password, hashedPassword);
+	}
+
+	public static String simpleHash(String data) {
+		try {
+			MessageDigest md = MessageDigest.getInstance(SHA_ALGORITHM);
+			byte[] encodedHash = md.digest(data.getBytes(StandardCharsets.UTF_8));
+			return new String(Hex.encodeHex(encodedHash));
+		} catch (NoSuchAlgorithmException e) {
+			LOG.log(Level.SEVERE, e.getMessage(), e);
+			return "";
+		}
+	}
 }
--- src/test/java/random/hash/HashUtilsTest.java
+++ src/test/java/random/hash/HashUtilsTest.java
@@ -11,29 +11,36 @@ import static org.junit.Assert.assertTrue;
 
 public class HashUtilsTest {
 
-    @Test
-    public void generateRandomSalt() {
-        byte[] salt = HashUtils.generateRandomSalt();
-        assertNotNull(salt);
-        System.out.println(DatatypeConverter.printHexBinary(salt));
-    }
+	@Test
+	public void generateRandomSalt() {
+		byte[] salt = HashUtils.generateRandomSalt();
+		assertNotNull(salt);
+		System.out.println("salt: " + DatatypeConverter.printHexBinary(salt));
+	}
 
-    @Test
-    public void createSha2Hash() throws Exception {
-        byte[] salt = HashUtils.generateRandomSalt();
-        String valueToHash = UUID.randomUUID().toString();
-        byte[] hash = HashUtils.createSha2Hash(valueToHash, salt);
-        assertNotNull(hash);
-        byte[] hash2 = HashUtils.createSha2Hash(valueToHash, salt);
-        assertEquals(DatatypeConverter.printHexBinary(hash), DatatypeConverter.printHexBinary(hash2));
-    }
+	@Test
+	public void createSha2Hash() throws Exception {
+		byte[] salt = HashUtils.generateRandomSalt();
+		String valueToHash = UUID.randomUUID().toString();
+		byte[] hash = HashUtils.createSha2Hash(valueToHash, salt);
+		assertNotNull(hash);
+		byte[] hash2 = HashUtils.createSha2Hash(valueToHash, salt);
+		assertEquals(DatatypeConverter.printHexBinary(hash), DatatypeConverter.printHexBinary(hash2));
+	}
 
-    @Test
-    public void testHashPassword() {
-        String password = "Good Password";
-        String passwordHash = HashUtils.hashPassword(password);
-        System.out.println(passwordHash);
-        assertTrue(HashUtils.verifyPassword(password, passwordHash));
-    }
+	@Test
+	public void testHashPassword() {
+		String password = "Good Password";
+		String passwordHash = HashUtils.hashPassword(password);
+		System.out.println("testHashPassword: " + passwordHash);
+		assertTrue(HashUtils.verifyPassword(password, passwordHash));
+	}
+
+	@Test
+	public void testSimpleHash() {
+		String password = "Good Password";
+		String passwordHash = HashUtils.simpleHash(password);
+		System.out.println("testSimpleHash: " + passwordHash);
+	}
 
 }
